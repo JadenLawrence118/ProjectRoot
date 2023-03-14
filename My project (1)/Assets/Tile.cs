493ff82e771a0f4822782d72b3b59603c3300652
nick <Nicholas2.Cornwell@live.uwe.ac.uk>
2023-03-14 20:45:40 +0000

    unity project

---
+++ My project (1)/Assets/Tile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject highlight;
+    [SerializeField] private GameObject player;
+
+
+    public void Init(bool isOffset) 
+    {
+        _renderer.color = isOffset ? _offsetColor : _baseColor;
+    }
+
+    void OnMouseEnter() 
+    {
+        {
+            highlight.SetActive(true);
+        }
+    }
+
+    void OnMouseExit() 
+    {
+        {
+            highlight.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+}
